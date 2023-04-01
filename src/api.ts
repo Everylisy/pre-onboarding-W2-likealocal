8f589b47d4fd2883b92e10b4bb4c6f5e364e336e
violetwhenisawu <anashape710@gmail.com>
2023-04-01 23:07:00 +0900

    refactor: api URL 경로 수정

--- src/api.ts
+++ src/api.ts
@@ -1,4 +1,4 @@
-const BASE_URL = `http://localhost:3000/data/data.json`;
+const BASE_URL = "/mock/data.json";
 
 export const fetchProducts = async () => {
   const response = await (await fetch(`${BASE_URL}`)).json();
